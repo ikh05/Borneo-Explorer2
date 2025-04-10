3b372c5c762035028b00d1519ee02439c350198b
ikh05 <mi3061923002@gmail.com>
2025-04-10 20:22:16 +0800

    a

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,18 @@
 <?php
 
-use App\Http\Controllers\GameController;
 use App\Models\Api;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
+use App\Http\Controllers\GameController;
 
 
 Route::get('/', [GameController::class, 'first']);
 Route::post('/', [GameController::class, 'start']);
+
+Route::get('/clear', function () {
+    Artisan::call('config:clear');
+    Artisan::call('cache:clear');
+    Artisan::call('view:clear');
+    Artisan::call('route:clear');
+    return 'Cache cleared';
+});
