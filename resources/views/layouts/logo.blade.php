dc5a82ab5743ea11b76db7c87b4f66ebdef8c5ad
ikh05 <mi3061923002@gmail.com>
2025-04-12 08:54:39 +0800

    fiks

--- resources/views/layouts/logo.blade.php
+++ resources/views/layouts/logo.blade.php
@@ -1,6 +1,6 @@
 <div id="logo_be" style="width: 100vw; height: 100vh; background-color: #121212; position: fixed; z-index: 5000000;">
     <div class="opacity-0">
-        <img src="img/logo/logo_be.svg" alt="" style="max-height: 80vh; max-width: 80vw; padding: 5rem; background-color: #242424; border: 3px blueviolet solid" class="position-absolute top-50 start-50 translate-middle rounded-circle">
+        <img src="img/logo/logo_be.svg" alt="" style="max-height: 80vh; max-width: 80vw; padding: 5rem;" class="position-absolute top-50 start-50 translate-middle rounded-circle">
         <div style="width: 90%; height: .7rem; border: 1px blueviolet solid; border-radius: .1rem" class="position-absolute bottom-0 mb-4 start-50 translate-middle-x d-flex align-items-center">
             <span style="height: 100%; max-width: 100%; background-color: #99bdd2;"></span>
             <img id="loading_kapal" src="img/logo/start_loading.png" style="width: 4rem; top: -2rem;" alt="" class="position-absolute">
