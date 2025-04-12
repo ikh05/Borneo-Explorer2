f9140816e0f28838498912354266a9c01566f788
ikh05 <mi3061923002@gmail.com>
2025-04-12 18:46:10 +0800

    info kabupaten android

--- resources/views/components/soal/info-kabupaten.blade.php
+++ resources/views/components/soal/info-kabupaten.blade.php
@@ -1,5 +1,5 @@
-<div class="info-kabupaten android-hidden">
-  <div class="bg-has end-0 rounded-start p-3 text-dark d-flex align-items-center" style="height: calc(100vh - 4rem); width: 23rem; position: static; margin-left: 2rem; background-image: img('img/logo/Kota Banjarmasin.png');">
+<div class="info-kabupaten d-none d-md-block">
+  <div class="bg-has end-0 rounded-start p-3 text-dark d-flex align-items-center" style="height: calc(100vh - 4rem); width: 23rem; position: static;">
       <div class="d-flex flex-column align-items-center">
         <h5 class="text-center mb-3">{{ $slot }}</h5>
         <div class="w-100 d-flex justify-content-center h-50 mb-2">
--- resources/views/components/soal/pilihan-soal.blade.php
+++ resources/views/components/soal/pilihan-soal.blade.php
@@ -1,7 +1,7 @@
-<div id="{{ Str::remove(' ', $name) }}" lokasi class="pilihan w-100 h-100 d-flex flex-reverse" style="padding-top: 4.5rem;">
+<div id="{{ Str::remove(' ', $name) }}" lokasi class="pilihan w-100 h-100 d-flex flex-reverse justify-content-between" style="padding-top: 4.5rem;">
         
     <!-- pilihan soal -->
-    <div class="d-flex flex-wrap puzzle-grid flex-shrink-0 align-items-between justify-content-between" style="width: calc(100% - 25rem);" >
+    <div class="d-flex flex-wrap puzzle-grid flex-shrink-0 align-items-between justify-content-between flex-grow-1" style=" margin-right: 2rem;" >
      @for ($i = 0; $i < 9; $i++)
         <x-card.soal name="{{ $name }}" x="{{ $i%3 }}" y="{{ floor($i/3) }}">{{ $i+1 }}</x-card.soal>
      @endfor
