f5ebc75e615794fd658f2259fb8e20afbba6a339
ikh05 <mi3061923002@gmail.com>
2025-04-17 05:17:16 +0800

    tes soal

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ Route::post('/simpan-soal', [GameController::class, 'simpanSoal']);
 Route::post('/ajax/simapnSoal', [GameController::class, 'simpanSoal']);
 
 Route::get('/a/{key}', function($key){
-    dd(Game::where('key', $key)->ger());
+    dd(Game::where('key', $key)->get());
     return view('a', ['data' => Game::where('key', $key)->soals]);
 });
